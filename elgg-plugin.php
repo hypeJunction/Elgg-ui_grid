29c7e77fca3e20b5fd87bd225bcd05d2b785ce17
ismayil.khayredinov@holoplot.com <ismayil.khayredinov@holoplot.com>
2026-04-07 15:59:22 +0200

    migrate(3.x): LLM-guided fixes for Elgg 3.x compatibility

---
+++ elgg-plugin.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+	'view_extensions' => [
+		'css/elements/grid' => [
+			'elements/ui/grid.css' => [],
+		],
+		'theme_sandbox/grid' => [
+			'theme_sandbox/ui/grid' => [],
+		],
+		'css/theme_sandbox.css' => [
+			'theme_sandbox/ui/grid.css' => [],
+		],
+	],
+];
